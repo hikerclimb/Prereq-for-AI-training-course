e0fb957cdeb3a50e0b0dc434adea9dbf9dd950a8
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-08-25 13:10:13 -0500

    got one more problem working :)

---
+++ Hands-On-Excercises-5_Module2.py
@@ -0,0 +1,5 @@
+import argparse
+parser = argparse.ArgumentParser("simple_example")
+parser.add_argument("counter", help="An integer will be increased by 1 and printed.", type=int)
+args = parser.parse_args()
+print(args.counter)
