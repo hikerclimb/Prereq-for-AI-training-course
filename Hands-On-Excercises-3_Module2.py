241c1681cad007f88ce28c8186d83d49495dddc7
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-08-25 13:00:30 -0500

    changed condition so or returns

--- Hands-On-Excercises-3_Module2.py
+++ Hands-On-Excercises-3_Module2.py
@@ -3,5 +3,5 @@ import pandas as pd
 df = pd.read_csv('Messy_Employee_dataset.csv')
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-filtered_data = df[(df['Age'].isna()) | (df['Salary'] == 'N/A')]
+filtered_data = df[(df['Age'].isna()) | (df['Salary'].isna())]
 print(filtered_data)
